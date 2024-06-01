33ea8c798349e632a592729febb4eece9e3ae93d
shahzaibintzar <shahzaibintzar27346@gmail.com>
2024-06-01 10:15:11 +0500

    electron js

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-const {app, BrowserWindow,} = require('electron')
-
+const {app, BrowserWindow,globalShortcut} = require('electron')
+const windowStateKeeper = require('electron-window-state')
+let win;
 function create()
 {
-    const win = new BrowserWindow({
-        width:400,
-        height:800,
+    let mainWindowState = windowStateKeeper({
+        defaultWidth:800,
+        defaultHeight:600
+    })
+     win = new BrowserWindow({
+
+        x:mainWindowState.x,
+        y:mainWindowState.y,
+        width:mainWindowState.width,
+        height:mainWindowState.height,
         // frame: false,
         // backgroundColor: '#FF0000',
         // alwaysOnTop: true,
@@ -17,7 +25,32 @@ function create()
     // let child = new BrowserWindow({parent:win})
     // child.loadFile('index.html')
     // child.show()
+
+
     win.loadFile('index.html')
+    // let wc = win.webContents;
+    // wc.on('dom-ready', ()=>{
+    //     console.log('This app Dom ready')
+    // })
+    // wc.on('did-finish-load', () =>{
+    //     console.log('This app did finish load')
+    // })
+    // wc.on('new-window', () =>{
+    //     console.log('new open window')
+    // })
+    // wc.on('before-input-event', ()=>{
+    //     console.log('before input event')
+    // })
+
+
+
+globalShortcut.register("alt+k", ()=>{
+    win.loadFile("child.html")
+})
+
+
+
+    mainWindowState.manage(win)
 
 }
 
@@ -42,6 +75,14 @@ function create()
 // app.on('browser-window-blur',(e)=>{
 //     console.log('Browser window blur')
 // })
+
+
+
+
+
+
+
+
 
 // app.whenReady().then(create);
 app.on('ready',() =>{
